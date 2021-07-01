a00fbe0b54a31a08a6944e2525f375a43f769bf0
pdiakumis <peterdiakumis@gmail.com>
2021-07-01 18:04:03 +1000

    setup R package skeleton

---
+++ R/utils_shortcuts.R
@@ -0,0 +1,8 @@
+#' Tidy eval helpers
+#'
+#' <https://www.tidyverse.org/blog/2019/06/rlang-0-4-0/>
+#'
+#' @name tidyeval
+#' @keywords internal
+#' @importFrom rlang .data
+NULL
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(cpgr)
+
+test_check("cpgr")
