9e0155b2fa71901ff181b3127928d3875608dfe3
pdiakumis <peterdiakumis@gmail.com>
2021-08-09 17:57:07 +1000

    fix bump script

--- bump.sh
+++ bump.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 v=$1
 
-commit_message="Bump v${v}"
+commit_message="Bump version: ${v}"
 
 sed -i "" "s/\(Version:\).*/\1 ${v}/" DESCRIPTION
 git add DESCRIPTION
 git commit -m "${commit_message}"
 git tag -a "${v}" -m "bump v${v}"
-git push --follow-tags
+git push origin "${v}"
